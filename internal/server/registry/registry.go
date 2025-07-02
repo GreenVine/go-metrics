74923f7c91f49cef145756feb6e3998b5888d961
Robin <commits@codemy.fun>
2025-07-02 20:13:50 +1000

    feat(server): init gRPC server implementation

---
+++ internal/server/registry/registry.go
@@ -0,0 +1,11 @@
+package registry
+
+import (
+	"github.com/greenvine/go-metrics/internal/server/service"
+	"github.com/greenvine/go-metrics/proto/gen/core/v1"
+	"google.golang.org/grpc"
+)
+
+func RegisterServices(s *grpc.Server) {
+	corev1.RegisterHealthServiceServer(s, service.NewHealthService())
+}
